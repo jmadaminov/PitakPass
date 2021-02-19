8deeb224ec40a6149a4a427e033e26830033410c
MJ <jahongir9779@mail.ru>
2021-02-19 17:06:41 +0500

    internet permission

--- app/src/main/java/com/badcompany/pitakpass/util/Constants.kt
+++ app/src/main/java/com/badcompany/pitakpass/util/Constants.kt
@@ -5,24 +5,17 @@ package com.badcompany.pitakpass.util
  */
 object Constants {
 
-    const val BASE_URL = "http://codeuz.uz:9091/"
+    const val BASE_URL = "http://178.218.207.92:9091/api/v1/"
+//    const val BASE_URL = "http://codeuz.uz:9091/"
     const val ROLE_PASSENGER = "PASSENGER"
     const val TXT_IS_FROM_POST_PREVIEW = "IS_FROM_POST_PREVIEW"
 
     const val errPhoneFormat = -99
-    const val TXT_CAR = "CAR"
     const val TXT_PASSENGER_POST = "PASSENGER_POST"
 
-    const val TXT_TOKEN = "TOKEN"
-    const val TXT_ID = "ID"
-    const val TXT_POSITION = "POSITION"
-    const val TXT_PLACE = "PLACE"
-    const val TXT_LANG = "LANGUAGE"
-    const val TXT_FILTER = "FILTER"
     const val TXT_PAGE = "PAGE"
     const val RU = "ru"
     const val EN = "en"
     const val UZ = "uz"
-    const val CODE_ADD_CAR = 99
 
 }
