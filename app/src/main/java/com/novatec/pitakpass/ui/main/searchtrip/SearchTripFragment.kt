34e3cebcdddccfbf7a8e8ea6c0441658745cfc50
MJ <jahongir9779@gmail.com>
2021-04-24 20:05:58 +0500

    some UI work

--- app/src/main/java/com/novatec/pitakpass/ui/main/searchtrip/SearchTripFragment.kt
+++ app/src/main/java/com/novatec/pitakpass/ui/main/searchtrip/SearchTripFragment.kt
@@ -315,6 +315,7 @@ class SearchTripFragment : Fragment(R.layout.fragment_search_trip) {
         )
 
         postsAdapter.addLoadStateListener { loadState ->
+            if (progress == null) return@addLoadStateListener
 //            swipeRefreshLayout.isRefreshing = loadState.source.refresh is LoadState.Loading
             progress.isVisible = loadState.source.refresh is LoadState.Loading
             rvPosts.isVisible = loadState.source.refresh is LoadState.NotLoading
